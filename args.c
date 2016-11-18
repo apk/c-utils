3c389b05aefe084967cb4a4ecd156bf21306af54
Andreas Krey <a.krey@gmx.de>
2016-11-18 17:36:17 +0100

    args: no quotes when certainly not needed

--- args.c
+++ args.c
@@ -4,31 +4,51 @@ main (int iArgc, char **ppszArgv) {
 	int i;
 	for (i = 0; i < iArgc; i ++) {
 		char *pc;
-		putchar ('\"');
 		for (pc = ppszArgv [i]; *pc; pc ++) {
 			int c = 0xff & *pc;
-			switch (c) {
-			case '\"':
-			case '\'':
-			case '\\':
-				printf ("\\%c", c);
-				break;
-			case '\n':
-				printf ("\\n");
-				break;
-			case '\t':
-				printf ("\\t");
-				break;
-			default:
-				if (c >= ' ' && c <= '~') {
-					putchar (c);
-				} else {
-					printf ("\\%03o", c);
-				}
+			if ((c >= 'a' && c <= 'z') ||
+			    (c >= 'A' && c <= 'Z') ||
+			    (c >= '0' && c <= '9') ||
+			    c == '-' || c == '=' ||
+			    c == '.' || c == '_' ||
+			    c == ',' || c == '+' ||
+			    c == ':' || c == '@') {
+			} else {
 				break;
 			}
 		}
-		putchar ('\"');
+		if (*pc) {
+			putchar ('\"');
+			for (pc = ppszArgv [i]; *pc; pc ++) {
+				int c = 0xff & *pc;
+				switch (c) {
+				case '\"':
+				case '\'':
+				case '\\':
+					printf ("\\%c", c);
+					break;
+				case '\n':
+					printf ("\\n");
+					break;
+				case '\t':
+					printf ("\\t");
+					break;
+				default:
+					if (c >= ' ' && c <= '~') {
+						putchar (c);
+					} else {
+						printf ("\\%03o", c);
+					}
+					break;
+				}
+			}
+			putchar ('\"');
+		} else {
+			for (pc = ppszArgv [i]; *pc; pc ++) {
+				int c = 0xff & *pc;
+				putchar (c);
+			}
+		}
 		putchar (i == iArgc - 1 ? '\n' : ' ');
 	}
 	return 0;
