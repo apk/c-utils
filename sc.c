b979d361b0375177ca1276903d98cf9277dc7caa
Andreas Krey <a.krey@gmx.de>
2025-11-03 16:08:48 +0100

    sc/unsc: accept filename args, like cat

--- sc.c
+++ sc.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
+#include "hdr.h"
 
-int main () {
+static void dofd (FILE *fd) {
   int b = 0;
   int c;
-  while ((c = getchar ()) != EOF) {
+  while ((c = getc (fd)) != EOF) {
     if (c >= ' ' && c <= '~') {
 #ifdef UN
       b=(c - ' ')-b;
@@ -20,5 +20,29 @@ int main () {
       putchar (c);
     }
   }
-  return 0;
+}
+
+int main (int argc, char **argv) {
+	int i;
+	for (i = 1; i < argc; i ++) {
+		char *a = argv [i];
+		char *p = a + 1;
+		if (*a != '-') break;
+		fprintf (stderr, "Bad opt '%s'\n", a);
+		exit (1);
+	}
+	if (i == argc) {
+		dofd (stdin);
+	} else {
+		for ( ; i < argc; i ++) {
+			FILE *fd = fopen (argv [i], "r");
+			if (!fd) {
+				perror ("open");
+				exit (1);
+			}
+			dofd (fd);
+			fclose (fd);
+		}
+	}
+	return 0;
 }
